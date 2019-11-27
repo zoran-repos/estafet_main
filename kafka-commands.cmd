9d59571f31a5e82d3c6479116320bb233fc452bf
zoran-train-path <trendo@hotmail.com>
2019-11-27 13:28:34 +0100

    Kafka starting commands

---
+++ kafka-commands.cmd
@@ -0,0 +1,7 @@
+- Starting kafka environment - Win ENV
+
+(Starting zookeeper)
+1. zookeeper-server-start.bat ..\..\config\zookeeper.properties
+
+(Starting broker)
+2. kafka-server-start.bat ..\..\config\server.properties
